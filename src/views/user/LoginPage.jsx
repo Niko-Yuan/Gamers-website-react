6d0fca3cbc7e29633c2a92a9d07b773c2e8bd3d9
Niko-Yuan <hairforcenikoyuan@gmail.com>
2023-09-06 15:51:27 -0700

    import styled

--- src/views/user/LoginPage.jsx
+++ src/views/user/LoginPage.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useRef } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Navigate, useNavigate  } from 'react-router-dom';
+import styled from "styled-components";
 
 import Form from "react-validation/build/form";
 import Input from "react-validation/build/input";
--- src/views/user/ProfilePage.jsx
+++ src/views/user/ProfilePage.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Navigate } from 'react-router-dom';
 import { useSelector } from "react-redux";
+import styled from "styled-components";
 
 const ProfilePage = () => {
   const { user: currentUser } = useSelector((state) => state.auth);
