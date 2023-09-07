82d2fd05d6a7d497ce9438b67294eaf56f7a15a6
Niko-Yuan <hairforcenikoyuan@gmail.com>
2023-09-06 22:06:01 -0700

    update login page style

--- src/views/user/LoginPage.jsx
+++ src/views/user/LoginPage.jsx
@@ -1,18 +1,23 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useRef, useCallback } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Navigate, useNavigate } from "react-router-dom";
 import styled from "styled-components";
-
+import Particles from "react-tsparticles";
+import { loadSlim } from "tsparticles-slim";
 import Form from "react-validation/build/form";
 import Input from "react-validation/build/input";
 import CheckButton from "react-validation/build/button";
-
 import { login } from "../../actions/auth";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faExclamationCircle } from "@fortawesome/free-solid-svg-icons";
 
 const required = (value) => {
   if (!value) {
     return (
-      <div className="alert alert-danger" role="alert">
+      <div className="custom-alert" role="alert">
+        <i className="alert-icon">
+          <FontAwesomeIcon icon={faExclamationCircle} />
+        </i>
         This field is required!
       </div>
     );
@@ -69,9 +74,92 @@ const LoginPage = () => {
     return <Navigate to="/home" />;
   }
 
+  const particlesInit = useCallback(async (engine) => {
+    // console.log(engine);
+    await loadSlim(engine);
+  }, []);
+
+  const particlesLoaded = useCallback(async (container) => {
+    await console.log(container);
+  }, []);
+
   return (
     <LoginPageWrapper>
-      <div className="container w-100">
+      <div className="login-page-card container w-100">
+        <Particles
+          id="tsparticles"
+          init={particlesInit}
+          loaded={particlesLoaded}
+          options={{
+            background: {
+              color: {
+                value: "#0d47a1",
+              },
+            },
+            fpsLimit: 120,
+            interactivity: {
+              events: {
+                onClick: {
+                  enable: true,
+                  mode: "push",
+                },
+                onHover: {
+                  enable: true,
+                  mode: "repulse",
+                },
+                resize: true,
+              },
+              modes: {
+                push: {
+                  quantity: 4,
+                },
+                repulse: {
+                  distance: 200,
+                  duration: 0.4,
+                },
+              },
+            },
+            particles: {
+              color: {
+                value: "#ffffff",
+              },
+              links: {
+                color: "#ffffff",
+                distance: 150,
+                enable: true,
+                opacity: 0.5,
+                width: 1,
+              },
+              move: {
+                direction: "none",
+                enable: true,
+                outModes: {
+                  default: "bounce",
+                },
+                random: false,
+                speed: 6,
+                straight: false,
+              },
+              number: {
+                density: {
+                  enable: true,
+                  area: 800,
+                },
+                value: 80,
+              },
+              opacity: {
+                value: 0.5,
+              },
+              shape: {
+                type: "circle",
+              },
+              size: {
+                value: { min: 1, max: 5 },
+              },
+            },
+            detectRetina: true,
+          }}
+        />
         <div className="card card-container">
           <img
             src="//ssl.gstatic.com/accounts/ui/avatar_2x.png"
@@ -105,7 +193,12 @@ const LoginPage = () => {
             </div>
 
             <div className="form-group">
-              <button className="btn btn-primary btn-block" disabled={loading}>
+              <button
+                type="submit"
+                aria-label="btn-login"
+                className="login-btn"
+                disabled={loading}
+              >
                 {loading && (
                   <span className="spinner-border spinner-border-sm"></span>
                 )}
@@ -115,12 +208,20 @@ const LoginPage = () => {
 
             {message && (
               <div className="form-group">
-                <div className="alert alert-danger" role="alert">
+                <div className="custom-alert" role="alert">
+                  <i className="alert-icon">
+                    <FontAwesomeIcon icon={faExclamationCircle} />
+                  </i>
                   {message}
                 </div>
               </div>
             )}
-            <CheckButton style={{ display: "none" }} ref={checkBtn} />
+            <CheckButton
+              type="button"
+              aria-label="login-form-submit-btn"
+              style={{ display: "none" }}
+              ref={checkBtn}
+            />
           </Form>
         </div>
       </div>
@@ -131,6 +232,22 @@ const LoginPage = () => {
 export default LoginPage;
 
 const LoginPageWrapper = styled.div`
+  #tsparticles {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    z-index: -1;
+  }
+
+  .login-page-card {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: relative;
+  }
+
   .card {
     background-color: #f7f7f7;
     padding: 20px 25px 30px;
@@ -145,8 +262,22 @@ const LoginPageWrapper = styled.div`
   }
 
   .card-container.card {
-    max-width: 350px !important;
     padding: 40px 40px;
+    background: linear-gradient(45deg, #fe6b8b 30%, #ff8e53 90%);
+    box-shadow: 0 3px 5px 2px rgba(255, 105, 135, 0.3);
+    transition: all 0.3s ease;
+
+    &:hover {
+      transform: scale(1.05);
+    }
+  }
+
+  .form-control {
+    width: 100%;
+    box-sizing: border-box;
+    height: 32px;
+    font-size: 18px;
+    padding: 10px;
   }
 
   .card-container {
@@ -160,11 +291,64 @@ const LoginPageWrapper = styled.div`
       -moz-border-radius: 50%;
       -webkit-border-radius: 50%;
       border-radius: 50%;
+      border: 3px solid white;
+      box-shadow: 0 2px 5px 1px rgba(0, 0, 0, 0.2);
     }
 
     label {
       display: block;
       margin-top: 10px;
+      color: white;
+      font-size: 22px;
+      font-weight: bold;
+    }
+  }
+
+  .login-btn {
+    background-color: white;
+    color: blue;
+    text-transform: uppercase;
+    border: 2px solid blue;
+    border-radius: 12px;
+    margin: 0 auto;
+    display: block;
+    margin-top: 10%;
+    transition: all 0.3s ease;
+    width: 50%;
+    height: 40px;
+    font-size: 18px;
+    padding: 5px 15px;
+
+    &:hover {
+      background-color: blue;
+      color: white;
+    }
+  }
+
+  .custom-alert {
+    background-color: #ff4d4d;
+    color: white;
+    padding: 10px;
+    border-radius: 5px;
+    display: flex;
+    align-items: center;
+    animation: slideIn 0.5s ease;
+    margin-top: 5px;
+  }
+
+  .alert-icon {
+    margin-right: 10px;
+    font-size: 20px;
+  }
+
+  @keyframes slideIn {
+    from {
+      opacity: 0;
+      transform: translateY(-20px);
+    }
+    to {
+      opacity: 1;
+      transform: translateY(0);
     }
   }
 `;
